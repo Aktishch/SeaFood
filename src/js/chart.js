2d042b0ab7785d7310c4e31c7972a46797c946d7
Александр Актищев <aktishch@gmail.com>
2023-02-09 12:08:28 +0300

    Why visit

--- src/js/chart.js
+++ src/js/chart.js
@@ -6,7 +6,7 @@ const init = () => {
     Chart.register(ChartDataLabels)
 
     const chartVisitors = document.getElementById('chart-visitors')
-    const chartSectors = document.getElementById('chart-sectors')
+    // const chartSectors = document.getElementById('chart-sectors')
 
     if (chartVisitors) {
 
@@ -44,41 +44,41 @@ const init = () => {
 
     }
 
-    if (chartSectors) {
+    // if (chartSectors) {
 
-        const chartContext = chartSectors.getContext('2d')
+    //     const chartContext = chartSectors.getContext('2d')
 
-        new Chart(chartContext, {
+    //     new Chart(chartContext, {
 
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    data: [24.0, 16.2, 15.5, 14.9, 13.0, 10.0, 6.4],
-                    backgroundColor: ['#00529C', '#2E3092', '#00A79E', '#00AEEF', '#662D91', '#F7941E', '#58595B'],
-                    borderWidth: 2,
-                    borderColor: '#ffffff'
-                }]
-            },
-            options: {
-                plugins: {
-                    datalabels: {
-                        color: '#ffffff',
-                        textAlign: 'center',
-                        font: {
-                            size: 16,
-                            weight: 700,
-                            lineHeight: 1.6
-                        },
-                        formatter: function (value, chartContext) {
-                            return value + '%';
-                        }
-                    }
-                }
-            }
+    //         type: 'doughnut',
+    //         data: {
+    //             datasets: [{
+    //                 data: [24.0, 16.2, 15.5, 14.9, 13.0, 10.0, 6.4],
+    //                 backgroundColor: ['#00529C', '#2E3092', '#00A79E', '#00AEEF', '#662D91', '#F7941E', '#58595B'],
+    //                 borderWidth: 2,
+    //                 borderColor: '#ffffff'
+    //             }]
+    //         },
+    //         options: {
+    //             plugins: {
+    //                 datalabels: {
+    //                     color: '#ffffff',
+    //                     textAlign: 'center',
+    //                     font: {
+    //                         size: 16,
+    //                         weight: 700,
+    //                         lineHeight: 1.6
+    //                     },
+    //                     formatter: function (value, chartContext) {
+    //                         return value + '%';
+    //                     }
+    //                 }
+    //             }
+    //         }
 
-        })
+    //     })
 
-    }
+    // }
 
 }
 

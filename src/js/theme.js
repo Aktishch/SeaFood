3d82c076cc123ae6f818edb01450cc9e44d10189
Александр Актищев <aktishch@gmail.com>
2023-05-02 16:10:09 +0300

    dark theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -17,15 +17,25 @@ let activeTheme;
 const init = () => {
   document.addEventListener('keyup', keyupHandler);
   window.addEventListener('unload', saveTheme);
+  window.addEventListener('change', changeHandler);
   initTheme();
 }
 
 const destroy = () => {
   document.removeEventListener('keyup', keyupHandler);
   window.removeEventListener('unload', saveTheme);
+  window.removeEventListener('change', changeHandler);
 }
 
-
+function changeHandler(event) {
+  if (event.target.closest('.-theme-changer-')) {
+    if (event.target.checked) {
+      setTheme(themes.dark);
+    } else {
+      setTheme(themes.clear);
+    }
+  }
+}
 
 function keyupHandler(event) {
   for (const i in themes) {
@@ -45,6 +55,15 @@ function saveTheme() {
 function setTheme(theme) {
   document.body.setAttribute('data-theme', theme.name);
   activeTheme = theme;
+
+  const changers = document.querySelectorAll('.-theme-changer-')
+  Array.from(changers).forEach(element => {
+    if (theme.name == 'dark') {
+      element.checked = true
+    } else {
+      element.checked = false
+    }
+  });
 }
 
 function initTheme() {

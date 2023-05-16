729fe751a4472a79fe0d3ed1acfdb1599581dbec
Александр Актищев <aktishch@gmail.com>
2023-05-16 13:54:55 +0300

    filter-slider

--- src/js/filter.js
+++ src/js/filter.js
@@ -26,11 +26,8 @@ const init = () => {
 
     if (filter) {
 
-        const line = filter.querySelector('.-filter-line-')
         const btns = filter.querySelectorAll('.-filter-item-')
         const cards = filter.querySelectorAll('.-filter-card-')
-
-        if (line) line.style.width = `${btns[0].offsetWidth}px`
 
         btns.forEach(btn => {
 
@@ -41,16 +38,6 @@ const init = () => {
 
                 currentBtn[0].className = currentBtn[0].className.replace('filter__item--active', '')
                 btn.className += ' filter__item--active'
-
-                const coordinates = {
-
-                    left: event.currentTarget.offsetLeft,
-                    width: event.currentTarget.offsetWidth,
-
-                }
-
-                line.style.width = `${coordinates.width}px`
-                line.style.left = `${coordinates.left}px`
 
                 filtering(currentCard, cards)
 
@@ -68,9 +55,6 @@ const init = () => {
 
                     currentBtn[0].className = currentBtn[0].className.replace('filter__item--active', '')
                     btns[index].classList.add('filter__item--active')
-
-                    line.style.left = `${btns[index].offsetLeft}px`
-                    line.style.width = `${btns[index].offsetWidth}px`
 
                     cards[0].classList.add('d-none')
                     card.classList.remove('d-none')
